4d83b6627d02b74ad6e69913f3ad1363bc2ac541
Christian Beam <beamchristian@yahoo.com>
2022-01-30 09:54:28 -0500

    projects array

--- client/src/components/ProjectsList/index.js
+++ client/src/components/ProjectsList/index.js
@@ -23,39 +23,3 @@ const ProjectsList = props => {
 };
 
 export default ProjectsList;
-
-// const [projects] = useState([
-//   {
-//     name: 'Deep Thoughts',
-//     category: 'portfolio',
-//     description: 'Full MERN Stack Application',
-//   },
-//   {
-//     name: 'My Tech Now',
-//     category: 'portfolio',
-//     description:
-//       'Fullstack stack application using Mysql, Express, and Handlebars',
-//   },
-//   {
-//     name: 'Oh Snap!',
-//     category: 'portfolio',
-//     description: 'This is Oh Snap! Front-end REACT application',
-//   },
-//   {
-//     name: 'Pizza Hunt',
-//     category: 'portfolio',
-//     description:
-//       'This is Pizza Hunt a Fullstack NoSql application using MongoDb and Express',
-//   },
-//   {
-//     name: 'Space Explorer',
-//     category: 'portfolio',
-//     description:
-//       'This is Space Explorer a Front end application using HTML, CSS, and JAVASCRIPT',
-//   },
-//   {
-//     name: 'Resume',
-//     category: 'resume',
-//     description: 'coming soon...',
-//   },
-// ]);
