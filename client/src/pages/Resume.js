83f4fa8be89cd83f5cf1b8cb12f6a621fb848039
beamchristian <ckbeamcoding@gmail.com>
2022-01-31 08:32:33 -0500

    resume iframe and download button

--- client/src/pages/Resume.js
+++ client/src/pages/Resume.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
+import ResumePdf from '../assets/resume/Christian-Beam-Resume.pdf';
+
 function Resume() {
   return (
-    <section className="my-5">
+    <section className="resume-section">
       <h1>Resume</h1>
-      <div className="my-2">
-        <p>My Resume</p>
+      <div className="download-resume">
+        <iframe
+          src="https://drive.google.com/file/d/1QsepJ_afeh9NbdHHMymM7E8LVSvt-1TfxWfGUt5pH_s/preview"
+          className="resume"
+          allow="autoplay"
+          title="resume-iframe"
+        ></iframe>
+        <a href={ResumePdf} download={'Christian-Beam-Resume'}>
+          <button className="resume-button">Download Christian's Resume</button>
+        </a>
       </div>
     </section>
   );
