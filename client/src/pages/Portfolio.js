3447a78e6945579b9f9d70efdcfee354e726a109
beamchristian <ckbeamcoding@gmail.com>
2022-02-09 21:49:29 -0500

    book-search added

--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -6,21 +6,13 @@ import Img2 from '../assets/small/portfolio/my-tech-now.jpg';
 import Img3 from '../assets/small/portfolio/Budget-Tracker.jpg';
 import Img4 from '../assets/small/portfolio/pizza-hunt.jpg';
 import Img5 from '../assets/small/portfolio/shop-shop.jpg';
-import img6 from '../assets/small/portfolio/versa-train.jpg';
+import img6 from '../assets/small/portfolio/book-search.jpg';
+import img7 from '../assets/small/portfolio/versa-train.jpg';
 
 const Portfolio = () => {
   const PROJECTS = [
     {
       id: 'p1',
-      name: 'Versa-Train',
-      description:
-        'Versa-Train, a full MERN Stack SPA(Single Page Application) using Apollo GraphQL. Its funtionality is for employers to create and host their training material so that their employees can stay up to date with required job training. Employers can create the employee profiles and set the employees login and password.  Once the employee is logged in, they have their own seperate dashboard. This project was done with 3 other contributers using Agile Methodology via github.',
-      image: img6,
-      repo: 'https://github.com/oldgraybuzzard/crispy-giggle',
-      deployment: 'https://crispy-gigglers.herokuapp.com/',
-    },
-    {
-      id: 'p6',
       name: 'Shop-Shop',
       description:
         'Shop-Shop, a e-commerce site where users can make an account, shop through an inventory of items, add them to a cart and checkout through the Stripe Api. Shop-Shop is a full MERN Stack SPA application using Apollo GraphQl and Redux for state. This application also has offline functionality through IndexedDb',
@@ -63,6 +55,24 @@ const Portfolio = () => {
       image: Img4,
       repo: 'https://github.com/beamchristian/pizza-hunt',
       deployment: 'https://fathomless-tundra-41161.herokuapp.com/',
+    },
+    {
+      id: 'p6',
+      name: 'Book Search Engine',
+      description:
+        'Book Search Engine, a full MERN Stack SPA(Single Page Application) using Apollo GraphQL. Users can create an account, search for any book listed in the Google BooksApi, and then save/delete their favorite books to their account.',
+      image: img6,
+      repo: 'https://github.com/beamchristian/book-search-graphql-refactor',
+      deployment: 'https://pacific-ocean-44039.herokuapp.com/',
+    },
+    {
+      id: 'p6',
+      name: 'Versa-Train (In Development)',
+      description:
+        'Versa-Train, a full MERN Stack SPA(Single Page Application) using Apollo GraphQL. Its funtionality is for employers to create and host their training material so that their employees can stay up to date with required job training. Employers can create the employee profiles and set the employees login and password.  Once the employee is logged in, they have their own seperate dashboard. This project was done with 3 other contributers using Agile Methodology via github.',
+      image: img7,
+      repo: 'https://github.com/oldgraybuzzard/crispy-giggle',
+      deployment: 'https://crispy-gigglers.herokuapp.com/',
     },
   ];
 
